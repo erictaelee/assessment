890fce344eb75d7692f562d2ed59d3dd17379c11
Eric <lee.erictae@gmail.com>
2021-05-27 00:55:31 -0500

    changed the css file name to remove error. Started working on slideshow on js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName('slide__bg');
+const totalSlides = slides.length;
+
+// console.log(slides);
+
+document.getElementById('slideshow__button--next')
+  .addEventListener("click", function () {
+    console.log("hellooooo");
+  })
